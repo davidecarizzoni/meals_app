74f87bb8a634f6a1abfe880a401a159efb34a163
davidecarizzoni <davide.carizzoni@runelab.it>
2023-08-29 22:38:31 +0200

    feat(tabs)

---
+++ lib/screens/tabs.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/meals.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({
+    super.key,
+  });
+
+  @override
+  State<TabsScreen> createState() {
+    return _TabsScreenState();
+  }
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  //_ this variables in in
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget selectedPage = const CategoriesScreen();
+    String selectedPageTitle = 'Your Categories';
+
+    if (_selectedPageIndex == 1) {
+      selectedPage = const MealsScreen(
+        title: 'favorites',
+        meals: [],
+      );
+      selectedPageTitle = 'Your Favorite';
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(selectedPageTitle),
+      ),
+      body: selectedPage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          )
+        ],
+      ),
+    );
+  }
+}
